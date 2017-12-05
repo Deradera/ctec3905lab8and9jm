f83667801ae005c8d9b49ad6a2f0c081ca8b83ae
Jojo Massey <P14152904@LEC-ADMIN.dmu.ac.uk>
2017-12-05 11:53:46 +0000

    added pics and stuff, need to get text back somehow :(

--- javascript/parse-data.js
+++ javascript/parse-data.js
@@ -8,23 +8,25 @@
   /*
     EXERCISE 01:
     Rewrite this to make it *loop* through all the lecturers' names.
-	
+
 	Clue: use "for key in..." and try to work out the syntax
     (Note the ES6 'template literal' syntax - you can add HTML tags)
   */
-  
+
   // set theData here to an empty string
-  
+  let theData = "";
   // create a loop around this line with "for ... in" (look it up)_
-  let theData = `${obj.lecturers[1].firstName} ${obj.lecturers[1].lastName}`;
+for (i in obj.lecturers) {
+  theData += `<li><img src="images/pusheen.gif" alt="${obj.lecturers[i].firstName}> ${obj.lecturers[i].lastName}"${obj.lecturers[i].firstName} ${obj.lecturers[i].lastName}</li>`;
+}
   // end the for loop here
-  
+
   // populates the chosen element's content with the data:
-  demoData.innerHTML = theData; 
+  demoData.innerHTML = theData;
 
   /*
     EXERCISE 02:
-    Once you have looped through all the data, present the 
+    Once you have looped through all the data, present the
     results on the page, styled with CSS.
   */
 }());
